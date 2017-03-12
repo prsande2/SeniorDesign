be9ae03482cdccde083467760d5d0f89ae766168
Mizumi Malhan <mmalhan@oakland.edu>
2017-03-11 21:51:31 -0500

    BrowseActivity now has S3 functionality, but it’s not working well. Different item photo than the intended one gets loaded. I think this has to do with creating a data race when starting to download the S3 image and then actually using it in the BrowseAdapter. Should set this up so that we only download from S3 once… that might fix it.

--- Rent_It_AndroidApp/app/src/main/java/com/rent_it_app/rent_it/BrowseActivity.java
+++ Rent_It_AndroidApp/app/src/main/java/com/rent_it_app/rent_it/BrowseActivity.java
@@ -1,6 +1,8 @@
 package com.rent_it_app.rent_it;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -8,10 +10,20 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.amazonaws.auth.CognitoCachingCredentialsProvider;
+import com.amazonaws.mobileconnectors.cognito.CognitoSyncManager;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferListener;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferObserver;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferState;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferUtility;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
 import com.google.gson.Gson;
 import com.rent_it_app.rent_it.firebase.Config;
 import com.rent_it_app.rent_it.json_models.Category;
@@ -19,7 +31,10 @@ import com.rent_it_app.rent_it.json_models.Item;
 import com.rent_it_app.rent_it.json_models.ItemEndpoint;
 import com.rent_it_app.rent_it.views.AvailabeItemFragment;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -37,15 +52,44 @@ public class BrowseActivity extends BaseActivity{
     private String category_id;
     private String category_name;
     private ArrayList<Item> brwsList;
+    private ArrayList<File> photoList;
     private ListView blist;
     Retrofit retrofit;
     ItemEndpoint itemEndpoint;
     Gson gson;
 
+    private CognitoCachingCredentialsProvider credentialsProvider;
+    private CognitoSyncManager syncClient;
+    private AmazonS3 s3;
+    private TransferUtility transferUtility;
+    private File imageFile;
+    private HashMap<Integer, File> imageHash;
+    private HashMap<Integer, ImageView> imageViewHash;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_browse);
+
+        photoList = new ArrayList<>();
+
+        imageHash = new HashMap<>();
+        imageViewHash = new HashMap<>();
+
+        credentialsProvider = new CognitoCachingCredentialsProvider(
+                getApplicationContext(),  // getApplicationContext(),
+                Constants.COGNITO_POOL_ID, // Identity Pool ID
+                Regions.US_WEST_2 // Region
+        );
+
+        // Initialize the Cognito Sync client
+        syncClient = new CognitoSyncManager(
+                getApplicationContext(),
+                Regions.US_WEST_2, // Region
+                credentialsProvider);
+
+        s3 = new AmazonS3Client(credentialsProvider);
+        transferUtility = new TransferUtility(s3, getApplicationContext());
 
         /*Intent intent = getIntent();
         String value = intent.getStringExtra("key");*/
@@ -75,6 +119,41 @@ public class BrowseActivity extends BaseActivity{
                 int statusCode = response.code();
                 //List<Item> items = response.body();
                 brwsList = response.body();
+
+                /*for(Item item : brwsList) {
+                    // load the bitmapList with the images
+                    File outputDir = getApplicationContext().getCacheDir(); // context being the Activity pointer
+                    try {
+                        File imageFile = File.createTempFile(item.getImage(), "", outputDir);
+                        imageFile.deleteOnExit();
+                        TransferObserver transferObserver =
+                                transferUtility.download(Constants.BUCKET_NAME, item.getImage(), imageFile);
+                        photoList.add(imageFile);
+                        Log.d("pos:id ", pos + ":" + transferObserver.getId());
+                        transferObserver.setTransferListener(new TransferListener() {
+                            @Override
+                            public void onStateChanged(int id, TransferState state) {
+                                if(state == TransferState.COMPLETED) {
+                                    //myPhoto.setImageResource(R.drawable.bg);
+                                    Bitmap myBitmap = BitmapFactory.decodeFile(imageHash.get(id).getAbsolutePath());
+                                    imageViewHash.get(id).setImageBitmap(Bitmap.createScaledBitmap(myBitmap, 200, 150, false));
+                                    Log.d("onStateChanged.id:", ""+id);
+                                }
+                            }
+
+                            public void onProgressChanged(int id, long bytesCurrent, long bytesTotal) {
+                                // Do something in the callback.
+                            }
+
+                            public void onError(int id, Exception e) {
+                                // Do something in the callback.
+                            }
+                        });
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }*/
+
 
                 //tv1.setText(sb.toString());
                 blist.setAdapter(new BrowseAdapter());
@@ -143,9 +222,42 @@ public class BrowseActivity extends BaseActivity{
             TextView title = (TextView) ll.findViewById(R.id.txtTitle); // get the child text view
             TextView city = (TextView) ll.findViewById(R.id.txtCity);
             TextView rate = (TextView) ll.findViewById(R.id.txtRate);
+            ImageView photo = (ImageView) ll.findViewById(R.id.photo);
             title.setText(c.getTitle());
             city.setText(c.getCity());
             rate.setText("$" + c.getRate() + " /day");
+            File outputDir = getApplicationContext().getCacheDir(); // context being the Activity pointer
+
+            try {
+                File imageFile = File.createTempFile(c.getImage(), "", outputDir);
+                imageFile.deleteOnExit();
+                TransferObserver transferObserver =
+                        transferUtility.download(Constants.BUCKET_NAME, c.getImage(), imageFile);
+                imageHash.put(transferObserver.getId(), imageFile);
+                imageViewHash.put(transferObserver.getId(), photo);
+                Log.d("pos:id ", pos + ":" + transferObserver.getId());
+                transferObserver.setTransferListener(new TransferListener() {
+                    @Override
+                    public void onStateChanged(int id, TransferState state) {
+                        if(state == TransferState.COMPLETED) {
+                            //myPhoto.setImageResource(R.drawable.bg);
+                            Bitmap myBitmap = BitmapFactory.decodeFile(imageHash.get(id).getAbsolutePath());
+                            imageViewHash.get(id).setImageBitmap(Bitmap.createScaledBitmap(myBitmap, 200, 150, false));
+                            Log.d("onStateChanged.id:", ""+id);
+                        }
+                    }
+
+                    public void onProgressChanged(int id, long bytesCurrent, long bytesTotal) {
+                        // Do something in the callback.
+                    }
+
+                    public void onError(int id, Exception e) {
+                        // Do something in the callback.
+                    }
+                });
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
             return v;
         }
